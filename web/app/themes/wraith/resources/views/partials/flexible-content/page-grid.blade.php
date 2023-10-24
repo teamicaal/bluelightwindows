f6607d532fa416a7b78dfa4f819540122b014b94
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-10-24 16:32:47 +0100

    Update page-grid.blade.php

--- web/app/themes/wraith/resources/views/partials/flexible-content/page-grid.blade.php
+++ web/app/themes/wraith/resources/views/partials/flexible-content/page-grid.blade.php
@@ -16,8 +16,8 @@ $title = get_sub_field('title');
                         $custom_title = get_sub_field('custom_title');
                     @endphp
 
-                        <div class="w-full md:w-1/2 lg:w-1/4 px-2 mb-4 product-link" style="flex: 0 0 auto;">
-                           <div class="embed-1by1 w-full relative rounded-sm overflow-hidden mb-6 lg:mb-0">
+                        <div class="w-full md:w-1/2 lg:w-1/3 px-2 mb-4 product-link" style="flex: 0 0 auto;">
+                           <div class="embed-1by1 cursor-pointer w-full relative rounded-sm overflow-hidden mb-6 lg:mb-0">
                               <img data-src="{{ $custom_image['url'] }}" src="{{ $custom_image['url'] }}" alt="{!! $custom_image['alt'] !!}" class="lozad object-fit-cover" />
                                 <div class="w-full  product-bottom-bg bottom-0 left-0 p-4">
                                   <h4 class="text-xl lg:text-lg text-white font-bold flex self-center relative z-10  mb-0 tracking-widest">{!! $custom_title !!}</h4>
