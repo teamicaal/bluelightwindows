ffcb441003ec0338cd3fd8aa009dfd979ed4a250
rgvaghani <33823707+rgvaghani@users.noreply.github.com>
2023-11-23 13:25:47 +0000

    delete site web file

--- web/app/themes/wraith/resources/views/partials/head.blade.php
+++ web/app/themes/wraith/resources/views/partials/head.blade.php
@@ -12,7 +12,6 @@ $script_schema_global = get_field('script_schema_global', 'option');
   <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
   <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
   <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
-  <link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
   <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#0076ff">
   <meta name="msapplication-TileColor" content="#0076ff">
   <meta name="theme-color" content="#ffffff">
